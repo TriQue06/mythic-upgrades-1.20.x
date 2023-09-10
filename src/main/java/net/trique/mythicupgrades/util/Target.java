19e93aebe843e222ce9e33548d5296c2e6960404
CyberSteve777 <slacoursedu@gmail.com>
2023-09-11 02:25:25 +0300

    made type final

--- src/main/java/net/trique/mythicupgrades/util/Target.java
+++ src/main/java/net/trique/mythicupgrades/util/Target.java
@@ -3,7 +3,7 @@ package net.trique.mythicupgrades.util;
 public enum Target {
     SELF(0),
     OTHERS(1);
-    private int type;
+    private final int type;
 
     Target(int type) {
         this.type = type;
