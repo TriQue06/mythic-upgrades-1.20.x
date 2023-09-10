d6258d5c48664a4ffcb44e1ea6ebd56b7a7b0ad9
CyberSteve777 <slacoursedu@gmail.com>
2023-09-11 01:50:49 +0300

    Added mixin for handling effects for items in main hand

---
+++ src/main/java/net/trique/mythicupgrades/mixin/LivingEntityMixin.java
@@ -0,0 +1,76 @@
+package net.trique.mythicupgrades.mixin;
+
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.trique.mythicupgrades.item.MythicItem;
+import net.trique.mythicupgrades.util.EffectMeta;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+
+
+    @Shadow
+    public abstract boolean hasStatusEffect(StatusEffect effect);
+
+
+    @Shadow public abstract boolean addStatusEffect(StatusEffectInstance effect);
+
+    @Shadow public abstract boolean removeStatusEffect(StatusEffect type);
+
+    @Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
+
+    @Shadow public abstract Map<StatusEffect, StatusEffectInstance> getActiveStatusEffects();
+
+    @Unique
+    private MythicItem lastUsed;
+
+    @Inject(method = "tick", at = @At(value = "HEAD"))
+    public void checkItemInHand(CallbackInfo ci) {
+        if (!this.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty() &&
+                (this.getEquippedStack(EquipmentSlot.MAINHAND).getItem() instanceof MythicItem item)) {
+            if (lastUsed != null) {
+                removeInfiniteEffects(lastUsed);
+            }
+            HashMap<StatusEffect, EffectMeta> effects = item.getForSelf();
+            lastUsed = item;
+            for (StatusEffect effect : effects.keySet()) {
+                if (effect != null && !effect.isInstant()) {
+                    EffectMeta meta = effects.get(effect);
+                    int duration = meta.getDuration();
+                    int amplifier = meta.getAmplifier();
+                    boolean ambient = meta.isAmbient();
+                    boolean showIcon = meta.shouldShowIcon();
+                    boolean showParticles = meta.shouldShowParticles();
+                    this.addStatusEffect(new StatusEffectInstance(effect, duration, amplifier, ambient, showParticles, showIcon));
+                }
+            }
+        } else {
+            removeInfiniteEffects(lastUsed);
+        }
+    }
+
+    @Unique
+    public void removeInfiniteEffects(MythicItem item) {
+        if (item != null) {
+            HashMap<StatusEffect, EffectMeta> effects = item.getForSelf();
+            for (StatusEffect effect : effects.keySet()) {
+                if (effect != null && this.hasStatusEffect(effect) && this.getActiveStatusEffects().get(effect).isInfinite()) {
+                    this.removeStatusEffect(effect);
+                }
+            }
+        }
+    }
+}
