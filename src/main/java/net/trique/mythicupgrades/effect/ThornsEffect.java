19200a23a26fcf9de8fcc2c9bac706a3d76fde0a
CyberSteve777 <slacoursedu@gmail.com>
2023-09-11 02:29:14 +0300

    made registerBlockItem inline

--- src/main/java/net/trique/mythicupgrades/effect/ThornsEffect.java
+++ src/main/java/net/trique/mythicupgrades/effect/ThornsEffect.java
@@ -2,9 +2,6 @@ package net.trique.mythicupgrades.effect;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageType;
-import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
--- src/main/java/net/trique/mythicupgrades/util/ArmorEffectsList.java
+++ src/main/java/net/trique/mythicupgrades/util/ArmorEffectsList.java
@@ -5,17 +5,7 @@ import net.minecraft.entity.effect.StatusEffect;
 import java.util.HashMap;
 import java.util.Set;
 
-public class ArmorEffectsList {
-    private final HashMap<StatusEffect, EffectMeta> allEffects;
-
-
-    public ArmorEffectsList() {
-        allEffects = new HashMap<>();
-    }
-
-    public ArmorEffectsList(HashMap<StatusEffect, EffectMeta> allEffects) {
-        this.allEffects = allEffects;
-    }
+public record ArmorEffectsList(HashMap<StatusEffect, EffectMeta> allEffects) {
 
     public Set<StatusEffect> getEffectsSet() {
         return allEffects.keySet();
@@ -23,9 +13,5 @@ public class ArmorEffectsList {
 
     public EffectMeta getAmplifierByEffect(StatusEffect effect) {
         return allEffects.get(effect);
-    }
-
-    public HashMap<StatusEffect, EffectMeta> getAllEffects() {
-        return allEffects;
     }
 }
