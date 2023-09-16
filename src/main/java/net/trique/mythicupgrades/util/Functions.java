749921198ee31e07c5d78ed082f8b5da553fb2f4
CyberSteve777 <slacoursedu@gmail.com>
2023-09-16 23:04:17 +0300

    Removed unnecessary functions and replaced with some that can help in future

--- src/main/java/net/trique/mythicupgrades/util/Functions.java
+++ src/main/java/net/trique/mythicupgrades/util/Functions.java
@@ -1,48 +1,10 @@
 package net.trique.mythicupgrades.util;
 
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.trique.mythicupgrades.item.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 
 public class Functions {
-    public static boolean isMythicItem(Item item) {
-        return item instanceof BaseMythicItem;
-    }
-
-
-    public static boolean checkCorrectArmorOn(ArmorMaterial material, LivingEntity entity) {
-        boolean result = true;
-        ArrayList<EquipmentSlot> slots = new ArrayList<>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET));
-        for (EquipmentSlot slot : slots) {
-            if (entity.getEquippedStack(slot).isEmpty()) {
-                return false;
-            } else {
-                Item item = entity.getEquippedStack(slot).getItem();
-                try {
-                    ArmorMaterial m = ((ArmorItem) item).getMaterial();
-                    result &= m.equals(material);
-                } catch (Exception e) {
-                    return false;
-                }
-            }
-        }
-        return result;
-    }
-
-    public static boolean hasMythicArmorOn(LivingEntity entity) {
-        boolean result = false;
-        ArrayList<EquipmentSlot> slots = new ArrayList<>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET));
-        for (EquipmentSlot slot: slots) {
-            if (!entity.getEquippedStack(slot).isEmpty()) {
-                result |= (entity.getEquippedStack(slot).getItem() instanceof MythicEffectsArmorItem);
-            }
-        }
-        return result;
+    public static ClientPlayerEntity getLocalPlayer() {
+        return MinecraftClient.getInstance().player;
     }
 }
