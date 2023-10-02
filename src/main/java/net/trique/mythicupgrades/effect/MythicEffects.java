163226440e8fdcf89012ae82e50879a1a24e2535
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2023-10-03 02:10:43 +0300

    added missing "}"... again...

--- src/main/java/net/trique/mythicupgrades/effect/MythicEffects.java
+++ src/main/java/net/trique/mythicupgrades/effect/MythicEffects.java
@@ -7,3 +7,4 @@ public class MythicEffects {
     public static StatusEffect POISONOUS_THORNS = new PoisonousThornsEffect(StatusEffectCategory.BENEFICIAL, 4699647);
     public static StatusEffect FREEZE = new FreezeEffect(StatusEffectCategory.HARMFUL, 0x45c1e0);
     public static StatusEffect DAMAGE_DEFLECTION = new DamageDeflectionEffect(StatusEffectCategory.BENEFICIAL, 4699647);
+}
