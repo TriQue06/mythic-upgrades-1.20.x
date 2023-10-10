1e00a99f82d8bbf4b2bebe38c41a909661eb8b4c
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2023-10-10 23:10:08 +0300

    Nerfed poison by PoisonousThornsEffect to IV and reduced its time to 60 ticks (3 seconds)

--- src/main/java/net/trique/mythicupgrades/effect/PoisonousThornsEffect.java
+++ src/main/java/net/trique/mythicupgrades/effect/PoisonousThornsEffect.java
@@ -22,7 +22,7 @@ public class PoisonousThornsEffect extends StatusEffect {
                     entity.damage(livingEntity.getWorld().getDamageSources().thorns(livingEntity), 1 + amplifier);
                     entity.playSound(SoundEvents.ENCHANT_THORNS_HIT, 1.0f, livingEntity.getSoundPitch());
                     entity.playSound(SoundEvents.ENTITY_SPIDER_HURT, 1.0f, livingEntity.getSoundPitch());
-                    entity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 80, 1 + amplifier), livingEntity);
+                    entity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 60, amplifier), livingEntity);
                 }
             }
         }
