7b4273ecffc33470ce76488766d394ced6510b60
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2023-10-17 23:49:02 +0300

    fixed logic of infinite effects

--- src/main/java/net/trique/mythicupgrades/mixin/LivingEntityMixin.java
+++ src/main/java/net/trique/mythicupgrades/mixin/LivingEntityMixin.java
@@ -4,12 +4,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.trique.mythicupgrades.item.BaseMythicItem;
 import net.trique.mythicupgrades.item.MythicEffectsArmorItem;
-import net.trique.mythicupgrades.util.EffectMeta;
 import net.trique.mythicupgrades.util.CommonFunctions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,23 +16,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
     @Shadow
-    public abstract boolean hasStatusEffect(StatusEffect effect);
-
-    @Shadow
-    public abstract boolean removeStatusEffect(StatusEffect type);
-
-    @Shadow
     public abstract ItemStack getEquippedStack(EquipmentSlot slot);
 
-    @Shadow
-    public abstract Map<StatusEffect, StatusEffectInstance> getActiveStatusEffects();
     @Unique
     private BaseMythicItem lastUsed;
 
@@ -46,13 +34,14 @@ public abstract class LivingEntityMixin {
     public void checkItemInHand(CallbackInfo ci) {
         if (!this.getEquippedStack(EquipmentSlot.MAINHAND).isEmpty() &&
                 (this.getEquippedStack(EquipmentSlot.MAINHAND).getItem() instanceof BaseMythicItem item)) {
-            if (lastUsed != null) {
-                removeMythicInfiniteEffects(lastUsed.getMainHandEffects());
+            if (!Objects.equals(lastUsed, item) || CommonFunctions.checkStatusEffects((LivingEntity) (Object) this,
+                    item.getMainHandEffects())) {
+                CommonFunctions.addStatusEffects((LivingEntity) (Object) this, item.getMainHandEffects());
             }
             lastUsed = item;
-            CommonFunctions.addStatusEffects((LivingEntity) (Object) this, item.getMainHandEffects());
         } else if (lastUsed != null) {
-            removeMythicInfiniteEffects(lastUsed.getMainHandEffects());
+            CommonFunctions.removeMythicInfiniteEffects((LivingEntity) (Object) this, lastUsed.getMainHandEffects());
+            lastUsed = null;
         }
     }
 
@@ -61,13 +50,14 @@ public abstract class LivingEntityMixin {
         ItemStack head = this.getEquippedStack(EquipmentSlot.HEAD);
         if (!head.isEmpty() && head.getItem() instanceof MythicEffectsArmorItem item &&
                 CommonFunctions.hasCorrectArmorOn((LivingEntity) (Object) this, item.getMaterial())) {
-            if (lastWorn != null) {
-                removeMythicInfiniteEffects(lastWorn.getEquipmentBuffs());
+            if (!Objects.equals(lastWorn, item) || CommonFunctions.checkStatusEffects((LivingEntity) (Object) this,
+                    item.getEquipmentBuffs())) {
+                CommonFunctions.addStatusEffects((LivingEntity) (Object) this, item.getEquipmentBuffs());
             }
             lastWorn = item;
-            CommonFunctions.addStatusEffects((LivingEntity) (Object) this, item.getEquipmentBuffs());
         } else if (lastWorn != null) {
-            removeMythicInfiniteEffects(lastWorn.getEquipmentBuffs());
+            CommonFunctions.removeMythicInfiniteEffects((LivingEntity) (Object) this, lastWorn.getEquipmentBuffs());
+            lastWorn = null;
         }
     }
 
@@ -80,17 +70,8 @@ public abstract class LivingEntityMixin {
                 ItemStack head = this.getEquippedStack(EquipmentSlot.HEAD);
                 if (!head.isEmpty() && head.getItem() instanceof MythicEffectsArmorItem item &&
                         CommonFunctions.hasCorrectArmorOn((LivingEntity) (Object) this, item.getMaterial())) {
-                    CommonFunctions.addStatusEffects(entity, item.getEquipmentDebuffs(), entity);
+                    CommonFunctions.addStatusEffects(entity, item.getEquipmentDebuffs(), (LivingEntity) (Object) this);
                 }
-            }
-        }
-    }
-
-    @Unique
-    public void removeMythicInfiniteEffects(HashMap<StatusEffect, EffectMeta> effects) {
-        for (StatusEffect effect : effects.keySet()) {
-            if (effect != null && this.hasStatusEffect(effect) && this.getActiveStatusEffects().get(effect).isInfinite()) {
-                this.removeStatusEffect(effect);
             }
         }
     }
