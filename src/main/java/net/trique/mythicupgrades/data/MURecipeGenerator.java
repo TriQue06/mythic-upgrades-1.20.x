fcd3a09ac04e823a2c9ef6ce53ec4aef45e53f1e
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2023-10-26 11:32:49 +0300

    Updated datagen classes

--- src/main/java/net/trique/mythicupgrades/data/MURecipeGenerator.java
+++ src/main/java/net/trique/mythicupgrades/data/MURecipeGenerator.java
@@ -7,7 +7,8 @@ import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.trique.mythicupgrades.block.MythicBlocks;
 import net.trique.mythicupgrades.registry.RegisterMythicItems;
-import static net.trique.mythicupgrades.data.MURecipeProvider.*;
+import static net.trique.mythicupgrades.util.DataGenHelper.*;
+import static net.trique.mythicupgrades.util.CommonFunctions.getId;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -48,18 +49,18 @@ public class MURecipeGenerator extends FabricRecipeProvider {
         offerBlasting(exporter, List.of(MythicBlocks.AQUAMARINE_ORE, MythicBlocks.DEEPSLATE_AQUAMARINE_ORE), RecipeCategory.MISC, RegisterMythicItems.AQUAMARINE,
                 3.0f, 100, "aquamarine");
 
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.SAPPHIRE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.SAPPHIRE_BLOCK,
-                "sapphire_block", "sapphire", "sapphire_ingot_from_sapphire_block", "sapphire");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.RUBY_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.RUBY_BLOCK,
-                "ruby_block", "ruby", "ruby_ingot_from_ruby_block", "ruby");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.JADE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.JADE_BLOCK,
-                "jade_block", "jade", "jade_ingot_from_jade_block", "jade");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.TOPAZ_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.TOPAZ_BLOCK,
-                "topaz_block", "topaz", "topaz_ingot_from_topaz_block", "topaz");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.AMETRINE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.AMETRINE_BLOCK,
-                "ametrine_block", "ametrine", "ametrine_ingot_from_ametrine_block", "ametrine");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, RegisterMythicItems.AQUAMARINE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.AQUAMARINE_BLOCK,
-                "aquamarine_block", "aquamarine", "aquamarine_ingot_from_aquamarine_block", "aquamarine");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.SAPPHIRE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.SAPPHIRE_BLOCK,
+                getId("sapphire_block"), "sapphire_block", getId("sapphire_ingot_from_sapphire_block"), "sapphire_ingot");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.RUBY_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.RUBY_BLOCK,
+                getId("ruby_block"), "ruby_block", getId("ruby_ingot_from_ruby_block"), "ruby_ingot");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.JADE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.JADE_BLOCK,
+                getId("jade_block"), "jade_block", getId("jade_ingot_from_jade_block"), "jade_ingot");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.TOPAZ_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.TOPAZ_BLOCK,
+                getId("topaz_block"), "topaz_block", getId("topaz_ingot_from_topaz_block"), "topaz_ingot");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.AMETRINE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.AMETRINE_BLOCK,
+                getId("ametrine_block"), "ametrine_block", getId("ametrine_ingot_from_ametrine_block"), "ametrine_ingot");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, RegisterMythicItems.AQUAMARINE_INGOT, RecipeCategory.BUILDING_BLOCKS, MythicBlocks.AQUAMARINE_BLOCK,
+                getId("aquamarine_block"), "aquamarine_block", getId("aquamarine_ingot_from_aquamarine_block"), "aquamarine_ingot");
 
         offerCustomSmithingTemplateCopyingRecipe(exporter, ()->RegisterMythicItems.AMETRINE_UPGRADE_SMITHING_TEMPLATE,
                 ()->RegisterMythicItems.AMETRINE, ()->Items.END_STONE);
@@ -74,31 +75,43 @@ public class MURecipeGenerator extends FabricRecipeProvider {
         offerCustomSmithingTemplateCopyingRecipe(exporter, ()->RegisterMythicItems.TOPAZ_UPGRADE_SMITHING_TEMPLATE,
                 ()->RegisterMythicItems.TOPAZ, ()->Items.SANDSTONE);
 
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.AMETRINE_INGOT, List.of(RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "ametrine", 2);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.AQUAMARINE_INGOT, List.of(RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "aquamarine", 2);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.RUBY_INGOT, List.of(RegisterMythicItems.RUBY, RegisterMythicItems.RUBY, RegisterMythicItems.RUBY, RegisterMythicItems.RUBY,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "ruby", 2);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.SAPPHIRE_INGOT, List.of(RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "sapphire", 2);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.TOPAZ_INGOT, List.of(RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "topaz", 2);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.JADE_INGOT, List.of(RegisterMythicItems.JADE, RegisterMythicItems.JADE, RegisterMythicItems.JADE, RegisterMythicItems.JADE,
-                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT), "jade", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.AMETRINE_INGOT, List.of(RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE, RegisterMythicItems.AMETRINE,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.AMETRINE),"ametrine_ingot", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.AQUAMARINE_INGOT, List.of(RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE, RegisterMythicItems.AQUAMARINE,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.AQUAMARINE),"aquamarine_ingot", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.RUBY_INGOT, List.of(RegisterMythicItems.RUBY, RegisterMythicItems.RUBY, RegisterMythicItems.RUBY, RegisterMythicItems.RUBY,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.RUBY),"ruby_ingot", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.SAPPHIRE_INGOT, List.of(RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE, RegisterMythicItems.SAPPHIRE,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.SAPPHIRE), "sapphire_ingot", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.TOPAZ_INGOT, List.of(RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ, RegisterMythicItems.TOPAZ,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.TOPAZ),"topaz_ingot", 2);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.JADE_INGOT, List.of(RegisterMythicItems.JADE, RegisterMythicItems.JADE, RegisterMythicItems.JADE, RegisterMythicItems.JADE,
+                Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT, Items.NETHERITE_INGOT),
+                List.of(RegisterMythicItems.JADE), "jade_ingot", 2);
         
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_APPOMATTOX, 
-                List.of(RegisterMythicItems.BROKEN_AMETRINE_DISC, RegisterMythicItems.AMETRINE), "ametrine", 1);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_FLOW_OF_THE_ABYSS, 
-                List.of(RegisterMythicItems.BROKEN_AQUAMARINE_DISC, RegisterMythicItems.AQUAMARINE), "aquamarine", 1);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_NELUMBO, 
-                List.of(RegisterMythicItems.BROKEN_JADE_DISC, RegisterMythicItems.JADE), "jade", 1);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_TANKER_ON_THE_LEVANTINES, 
-                List.of(RegisterMythicItems.BROKEN_RUBY_DISC, RegisterMythicItems.RUBY), "ruby", 1);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_SOOT, 
-                List.of(RegisterMythicItems.BROKEN_SAPPHIRE_DISC, RegisterMythicItems.SAPPHIRE), "sapphire", 1);
-        offerShapelessRecipeWithMultipleInputs(exporter, ()->RegisterMythicItems.MUSIC_DISC_FIERCE, 
-                List.of(RegisterMythicItems.BROKEN_TOPAZ_DISC, RegisterMythicItems.TOPAZ), "topaz", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_APPOMATTOX,
+                List.of(RegisterMythicItems.BROKEN_AMETRINE_DISC, RegisterMythicItems.AMETRINE),
+                List.of(RegisterMythicItems.BROKEN_AMETRINE_DISC), "mu_music_disc", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_FLOW_OF_THE_ABYSS,
+                List.of(RegisterMythicItems.BROKEN_AQUAMARINE_DISC, RegisterMythicItems.AQUAMARINE),
+                List.of(RegisterMythicItems.BROKEN_AQUAMARINE_DISC), "mu_music_disc", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_NELUMBO,
+                List.of(RegisterMythicItems.BROKEN_JADE_DISC, RegisterMythicItems.JADE),
+                List.of(RegisterMythicItems.BROKEN_JADE_DISC),"mu_music_disc", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_TANKER_ON_THE_LEVANTINES,
+                List.of(RegisterMythicItems.BROKEN_RUBY_DISC, RegisterMythicItems.RUBY),
+                List.of(RegisterMythicItems.BROKEN_RUBY_DISC),"mu_music_disc", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_SOOT,
+                List.of(RegisterMythicItems.BROKEN_SAPPHIRE_DISC, RegisterMythicItems.SAPPHIRE),
+                List.of(RegisterMythicItems.BROKEN_SAPPHIRE_DISC),"mu_music_disc", 1);
+        offerShapelessRecipeWithMultipleInputsAndKeyItems(exporter, ()->RegisterMythicItems.MUSIC_DISC_FIERCE,
+                List.of(RegisterMythicItems.BROKEN_TOPAZ_DISC, RegisterMythicItems.TOPAZ),
+                List.of(RegisterMythicItems.BROKEN_TOPAZ_DISC),"mu_music_disc", 1);
 
         offerCustomUpgradeRecipe(exporter, RegisterMythicItems.AMETRINE_UPGRADE_SMITHING_TEMPLATE,
                 Items.NETHERITE_AXE, RegisterMythicItems.AMETRINE_INGOT, RecipeCategory.COMBAT, RegisterMythicItems.AMETRINE_AXE);
