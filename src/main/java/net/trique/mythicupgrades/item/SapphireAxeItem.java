d883e4aedd86609e6f5c563cba9a0ee9c0507324
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2023-11-19 15:36:55 +0300

    added new classes for sapphire weapons

---
+++ src/main/java/net/trique/mythicupgrades/item/SapphireAxeItem.java
@@ -0,0 +1,36 @@
+package net.trique.mythicupgrades.item;
+
+import static net.trique.mythicupgrades.MythicUpgrades.CONFIG;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import net.trique.mythicupgrades.util.ItemEffectsList;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class SapphireAxeItem extends MythicEffectsAxeItem {
+
+    private int percent;
+
+    public SapphireAxeItem(int attackDamage, int percent,float attackSpeed, Settings settings, ItemEffectsList effects, String tooltipKey, Formatting color) {
+        super(MUToolMaterials.SAPPHIRE, attackDamage, attackSpeed, settings, effects, tooltipKey, color);
+        this.percent = percent;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        String tltp = Text.translatable(tooltipKey).getString().formatted(CONFIG.sapphireConfig.axe_percentage_damage_percent());
+        tooltip.add(Text.literal(tltp).formatted(color));
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+}
