ec6da9b3fda0d209d9724936ca8d9a6d55a4df7b
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2024-07-21 20:42:37 +0300

    updated template stuff

--- src/main/java/net/trique/mythicupgrades/item/MUTemplateItem.java
+++ src/main/java/net/trique/mythicupgrades/item/MUTemplateItem.java
@@ -6,11 +6,11 @@ import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.SmithingTemplateItem;
+import net.trique.mythicupgrades.MythicUpgrades;
 
 public class MUTemplateItem extends SmithingTemplateItem {
     private static final ChatFormatting TITLE_FORMATTING;
     private static final ChatFormatting DESCRIPTION_FORMATTING;
-    private static final String TRANSLATION_KEY;
     private static final ResourceLocation EMPTY_ARMOR_SLOT_HELMET_TEXTURE;
     private static final ResourceLocation EMPTY_ARMOR_SLOT_CHESTPLATE_TEXTURE;
     private static final ResourceLocation EMPTY_ARMOR_SLOT_LEGGINGS_TEXTURE;
@@ -21,10 +21,6 @@ public class MUTemplateItem extends SmithingTemplateItem {
     private static final ResourceLocation EMPTY_SLOT_SHOVEL_TEXTURE;
     private static final ResourceLocation EMPTY_SLOT_PICKAXE_TEXTURE;
     private static final ResourceLocation EMPTY_SLOT_INGOT_TEXTURE;
-    private final Component baseSlotDescriptionText;
-    private final Component additionsSlotDescriptionText;
-    private final List<ResourceLocation> emptyBaseSlotTextures;
-    private final List<ResourceLocation> emptyAdditionsSlotTextures;
 
     private static final Component PERIDOT_UPGRADE_TEXT;
     private static final Component PERIDOT_UPGRADE_APPLIES_TO_TEXT;
@@ -64,10 +60,6 @@ public class MUTemplateItem extends SmithingTemplateItem {
 
     public MUTemplateItem(Component appliesToText, Component ingredientsText, Component titleText, Component baseSlotDescriptionText, Component additionsSlotDescriptionText, List<ResourceLocation> emptyBaseSlotTextures, List<ResourceLocation> emptyAdditionsSlotTextures) {
         super(appliesToText, ingredientsText, titleText, baseSlotDescriptionText, additionsSlotDescriptionText, emptyBaseSlotTextures, emptyAdditionsSlotTextures);
-        this.baseSlotDescriptionText = baseSlotDescriptionText;
-        this.additionsSlotDescriptionText = additionsSlotDescriptionText;
-        this.emptyBaseSlotTextures = emptyBaseSlotTextures;
-        this.emptyAdditionsSlotTextures = emptyAdditionsSlotTextures;
     }
 
     public static MUTemplateItem createPeridotUpgrade() {
@@ -154,30 +146,9 @@ public class MUTemplateItem extends SmithingTemplateItem {
         return List.of(EMPTY_SLOT_INGOT_TEXTURE);
     }
 
-    public Component getBaseSlotDescription() {
-        return this.baseSlotDescriptionText;
-    }
-
-    public Component getAdditionSlotDescription() {
-        return this.additionsSlotDescriptionText;
-    }
-
-    public List<ResourceLocation> getBaseSlotEmptyIcons() {
-        return this.emptyBaseSlotTextures;
-    }
-
-    public List<ResourceLocation> getAdditionalSlotEmptyIcons() {
-        return this.emptyAdditionsSlotTextures;
-    }
-
-    public String getDescriptionId() {
-        return TRANSLATION_KEY;
-    }
-
     static {
         TITLE_FORMATTING = ChatFormatting.GRAY;
         DESCRIPTION_FORMATTING = ChatFormatting.BLUE;
-        TRANSLATION_KEY = Util.makeDescriptionId("item", new ResourceLocation("smithing_template"));
         EMPTY_ARMOR_SLOT_HELMET_TEXTURE = new ResourceLocation("item/empty_armor_slot_helmet");
         EMPTY_ARMOR_SLOT_CHESTPLATE_TEXTURE = new ResourceLocation("item/empty_armor_slot_chestplate");
         EMPTY_ARMOR_SLOT_LEGGINGS_TEXTURE = new ResourceLocation("item/empty_armor_slot_leggings");
@@ -189,46 +160,46 @@ public class MUTemplateItem extends SmithingTemplateItem {
         EMPTY_SLOT_PICKAXE_TEXTURE = new ResourceLocation("item/empty_slot_pickaxe");
         EMPTY_SLOT_INGOT_TEXTURE = new ResourceLocation(":item/empty_slot_ingot");
 
-        PERIDOT_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("peridot_upgrade"))).withStyle(TITLE_FORMATTING);
-        PERIDOT_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.peridot_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        PERIDOT_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.peridot_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        PERIDOT_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.peridot_upgrade.base_slot_description")));
-        PERIDOT_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.peridot_upgrade.additions_slot_description")));
+        PERIDOT_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "peridot_upgrade"))).withStyle(TITLE_FORMATTING);
+        PERIDOT_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.peridot_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        PERIDOT_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.peridot_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        PERIDOT_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.peridot_upgrade.base_slot_description")));
+        PERIDOT_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.peridot_upgrade.additions_slot_description")));
 
-        TOPAZ_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("topaz_upgrade"))).withStyle(TITLE_FORMATTING);
-        TOPAZ_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.topaz_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        TOPAZ_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.topaz_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        TOPAZ_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.topaz_upgrade.base_slot_description")));
-        TOPAZ_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.topaz_upgrade.additions_slot_description")));
+        TOPAZ_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "topaz_upgrade"))).withStyle(TITLE_FORMATTING);
+        TOPAZ_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.topaz_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        TOPAZ_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.topaz_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        TOPAZ_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.topaz_upgrade.base_slot_description")));
+        TOPAZ_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.topaz_upgrade.additions_slot_description")));
 
-        AQUAMARINE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("aquamarine_upgrade"))).withStyle(TITLE_FORMATTING);
-        AQUAMARINE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.aquamarine_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        AQUAMARINE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.aquamarine_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        AQUAMARINE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.aquamarine_upgrade.base_slot_description")));
-        AQUAMARINE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.aquamarine_upgrade.additions_slot_description")));
+        AQUAMARINE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "aquamarine_upgrade"))).withStyle(TITLE_FORMATTING);
+        AQUAMARINE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.aquamarine_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        AQUAMARINE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.aquamarine_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        AQUAMARINE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.aquamarine_upgrade.base_slot_description")));
+        AQUAMARINE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.aquamarine_upgrade.additions_slot_description")));
     
-        SAPPHIRE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("sapphire_upgrade"))).withStyle(TITLE_FORMATTING);
-        SAPPHIRE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.sapphire_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        SAPPHIRE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.sapphire_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        SAPPHIRE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.sapphire_upgrade.base_slot_description")));
-        SAPPHIRE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.sapphire_upgrade.additions_slot_description")));
+        SAPPHIRE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "sapphire_upgrade"))).withStyle(TITLE_FORMATTING);
+        SAPPHIRE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.sapphire_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        SAPPHIRE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.sapphire_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        SAPPHIRE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.sapphire_upgrade.base_slot_description")));
+        SAPPHIRE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.sapphire_upgrade.additions_slot_description")));
 
-        RUBY_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("ruby_upgrade"))).withStyle(TITLE_FORMATTING);
-        RUBY_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ruby_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        RUBY_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ruby_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        RUBY_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ruby_upgrade.base_slot_description")));
-        RUBY_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ruby_upgrade.additions_slot_description")));
+        RUBY_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "ruby_upgrade"))).withStyle(TITLE_FORMATTING);
+        RUBY_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ruby_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        RUBY_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ruby_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        RUBY_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ruby_upgrade.base_slot_description")));
+        RUBY_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ruby_upgrade.additions_slot_description")));
 
-        AMETRINE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("ametrine_upgrade"))).withStyle(TITLE_FORMATTING);
-        AMETRINE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ametrine_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        AMETRINE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ametrine_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        AMETRINE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ametrine_upgrade.base_slot_description")));
-        AMETRINE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.ametrine_upgrade.additions_slot_description")));
+        AMETRINE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "ametrine_upgrade"))).withStyle(TITLE_FORMATTING);
+        AMETRINE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ametrine_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        AMETRINE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ametrine_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        AMETRINE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ametrine_upgrade.base_slot_description")));
+        AMETRINE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.ametrine_upgrade.additions_slot_description")));
     
-        JADE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation("jade_upgrade"))).withStyle(TITLE_FORMATTING);
-        JADE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.jade_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
-        JADE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.jade_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
-        JADE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.jade_upgrade.base_slot_description")));
-        JADE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation("smithing_template.jade_upgrade.additions_slot_description")));
+        JADE_UPGRADE_TEXT = Component.translatable(Util.makeDescriptionId("upgrade", new ResourceLocation(MythicUpgrades.MOD_ID, "jade_upgrade"))).withStyle(TITLE_FORMATTING);
+        JADE_UPGRADE_APPLIES_TO_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.jade_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMATTING);
+        JADE_UPGRADE_INGREDIENTS_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.jade_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMATTING);
+        JADE_UPGRADE_BASE_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.jade_upgrade.base_slot_description")));
+        JADE_UPGRADE_ADDITIONS_SLOT_DESCRIPTION_TEXT = Component.translatable(Util.makeDescriptionId("item", new ResourceLocation(MythicUpgrades.MOD_ID, "smithing_template.jade_upgrade.additions_slot_description")));
     }
 }
