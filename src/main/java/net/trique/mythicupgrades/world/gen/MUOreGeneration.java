0a5eb2b28cd59cd7a7637aab1aef9f330605f820
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2024-07-21 20:13:47 +0300

    updated worldgen

--- src/main/java/net/trique/mythicupgrades/world/gen/MUOreGeneration.java
+++ src/main/java/net/trique/mythicupgrades/world/gen/MUOreGeneration.java
@@ -2,19 +2,19 @@ package net.trique.mythicupgrades.world.gen;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.trique.mythicupgrades.world.MUPlacedFeatures;
 
 public class MUOreGeneration {
     public static void generateOres() {
-        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.ICY),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.IS_ICY),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.AQUAMARINE_ORE_PLACED_KEY);
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.AQUAMARINE_CRYSTAL_GEODE_PLACED_KEY);
 
-        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.JUNGLE),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.IS_JUNGLE),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.PERIDOT_ORE_PLACED_KEY);
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.PERIDOT_CRYSTAL_GEODE_PLACED_KEY);
@@ -29,7 +29,7 @@ public class MUOreGeneration {
         BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.SAPPHIRE_CRYSTAL_GEODE_PLACED_KEY);
 
-        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.DESERT),
+        BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.IS_DESERT),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.TOPAZ_ORE_PLACED_KEY);
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
                 GenerationStep.Decoration.UNDERGROUND_ORES, MUPlacedFeatures.TOPAZ_CRYSTAL_GEODE_PLACED_KEY);
