9a4c67660515d4793da7e6af57349cfd1924155c
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2024-07-31 20:26:39 +0300

    fixed possible Null Pointer Exception with packets

--- src/main/java/net/trique/mythicupgrades/networking/MUPackets.java
+++ src/main/java/net/trique/mythicupgrades/networking/MUPackets.java
@@ -2,17 +2,19 @@ package net.trique.mythicupgrades.networking;
 
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.player.LocalPlayer;
 import net.trique.mythicupgrades.networking.packet.PercentAnimationPacket;
 import net.trique.mythicupgrades.registry.RegisterMUParticles;
 
 public class MUPackets {
 
     public static void registerS2CPackets() {
-        ClientPlayNetworking.registerGlobalReceiver(PercentAnimationPacket.PACKET_ID, (client, handler, buf, responseSender) -> {
-            client.execute(() -> {
-                client.particleEngine.createTrackingEmitter(client.player.level().getEntity(buf.readVarInt()),
+        ClientPlayNetworking.registerGlobalReceiver(PercentAnimationPacket.PACKET_ID, (client, handler, buf, responseSender) -> client.execute(() -> {
+            LocalPlayer localPlayer = client.player;
+            if (localPlayer != null && localPlayer.level().getEntity(buf.readVarInt()) != null) {
+                client.particleEngine.createTrackingEmitter(localPlayer.level().getEntity(buf.readVarInt()),
                         RegisterMUParticles.PERCENT_PARTICLE);
-            });
-        });
+            }
+        }));
     }
 }
