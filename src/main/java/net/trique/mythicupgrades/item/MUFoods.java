157aac54a5bc557f08880138b432cd90f7395be5
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2024-08-04 20:42:30 +0300

    updated potions to match 1.21

--- src/main/java/net/trique/mythicupgrades/item/MUFoods.java
+++ src/main/java/net/trique/mythicupgrades/item/MUFoods.java
@@ -7,9 +7,7 @@ import net.trique.mythicupgrades.effect.MUEffects;
 
 public class MUFoods {
     public static final FoodProperties AQUAMARINE_POTION = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.4F).
-            effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 3600, 0), 1.0F).
-            effect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 3600, 0), 1.0F).
-            effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 3600, 0), 0.5F).
+            effect(new MobEffectInstance(MUEffects.ICE_SHIELD, 3600, 0), 1.0F).
             alwaysEat().build();
 
 
@@ -33,8 +31,6 @@ public class MUFoods {
             alwaysEat().build();
 
     public static final FoodProperties AMETRINE_POTION = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.4F).
-            effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3600, 2), 1.0F).
-            effect(new MobEffectInstance(MobEffects.JUMP, 3600, 2), 1.0F).
             effect(new MobEffectInstance(MUEffects.ARCANE_AURA, 3600, 2), 1.0F).
             alwaysEat().build();
 
