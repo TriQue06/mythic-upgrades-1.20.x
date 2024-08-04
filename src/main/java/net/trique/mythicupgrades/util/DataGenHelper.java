ae4c0cba68f8e903e299a23e011d994d1538386a
CyberSteve777 <69720241+CyberSteve777@users.noreply.github.com>
2024-08-04 19:20:35 +0300

    Removed unused imports

--- src/main/java/net/trique/mythicupgrades/util/DataGenHelper.java
+++ src/main/java/net/trique/mythicupgrades/util/DataGenHelper.java
@@ -7,8 +7,6 @@ import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.PotionItem;
-import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.trique.mythicupgrades.MythicUpgrades;
