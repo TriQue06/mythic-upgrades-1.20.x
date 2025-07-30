ebb83ca36a738ab6e491f950dfdae9f4c3782c5f
TriQue06 <baristanlik@gmail.com>
2025-07-30 12:48:44 +0300

    Update MUPackets.java

--- src/main/java/net/trique/mythicupgrades/networking/MUPackets.java
+++ src/main/java/net/trique/mythicupgrades/networking/MUPackets.java
@@ -1,6 +1,5 @@
 package net.trique.mythicupgrades.networking;
 
-
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.player.LocalPlayer;
@@ -9,12 +8,9 @@ import net.trique.mythicupgrades.networking.packet.PercentAnimationPacket;
 import net.trique.mythicupgrades.registry.RegisterMUParticles;
 
 public class MUPackets {
-
-
     public static void registerPacketCodecs() {
         PayloadTypeRegistry.playS2C().register(PercentAnimationPacket.PACKET_ID, PercentAnimationPacket.PACKET_CODEC);
     }
-
 
     public static void registerS2CPackets() {
         ClientPlayNetworking.registerGlobalReceiver(PercentAnimationPacket.PACKET_ID, (payload, context) -> {
